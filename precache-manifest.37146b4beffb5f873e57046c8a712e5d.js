248ddc7cd7176fa508db796c8b35fecb22021834
vvirk <virkolomeo@gmail.com>
2020-09-07 00:00:34 +0300

    Updates

---
+++ precache-manifest.37146b4beffb5f873e57046c8a712e5d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "444ad504efa146b27527cc1bc6d0d39a",
+    "url": "/users-list/index.html"
+  },
+  {
+    "revision": "b714b4e210a59c5b18cf",
+    "url": "/users-list/static/css/main.b4e32d5d.chunk.css"
+  },
+  {
+    "revision": "dfa9f1b75f313abb03f5",
+    "url": "/users-list/static/js/2.480c126e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/users-list/static/js/2.480c126e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b714b4e210a59c5b18cf",
+    "url": "/users-list/static/js/main.597f7ece.chunk.js"
+  },
+  {
+    "revision": "f753896ff9a53f845d76",
+    "url": "/users-list/static/js/runtime-main.4489c7d9.js"
+  }
+]);
